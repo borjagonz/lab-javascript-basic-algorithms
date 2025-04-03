ec28b7638a8290baa3dc3a7d2eb4f0c7e501da6c
Borja González <borjagonz@gmail.com>
2025-04-03 11:11:31 +0200

    hola

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 // Iteration 1: Names and Input
 
+let hacker1 = "The driver's name is Jackson Williams";
+console.log (hacker1);
+
+let hacker2 = "The navigator's name is Peter Griffin";
+console.log (hacker2);
 
 // Iteration 2: Conditionals
 
+const hacker1Name = "Jackson Williams";
+console.log (hacker1Name.length);
+
+const hacker2Name = "Peter Griffin";
+console.log (hacker2Name.length);
+
+let longestName = "The driver has the longest name, it has 16 characters.";
+console.log (longestName);
 
 // Iteration 3: Loops
+
+let driver = "Jackson";
+console.log (driver.toUpperCase());
+
+let navigator = "Peter";
+let reversed = "";
+
+for (let i = navigator.length -1; i>= 0; i--) {
+    reversed += navigator[i];
+}
+console.log (reversed);
+
+let LexicOrder = "The driver's name goes first.";
+console.log (LexicOrder);
